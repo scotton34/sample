d2037f563e02a6dd8fee467e2dc874c8c9393f3a
scotton34 <scotton34@outlook.com>
2016-02-10 16:56:19 +0000

    Set default Class and start on Cart Class

---
+++ fuel/application/controllers/cart.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Description of cart
+ *
+ * @author Stephen Cotton <stephen.cotton@stoneacre.co.uk>
+ */
+class Cart extends CI_Controller {
+    public $amount;
+    
+    
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function init(){
+        
+    }
+    
+}
